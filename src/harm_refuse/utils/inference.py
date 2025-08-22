d525592a8b6d6bca20bb0d8edbd8a51b4383e8f4
jamie-stephenson <jamie.t.stephenson@gmail.com>
2025-08-23 00:20:48 +0100

    Continued refactoring dataset bulding
    Progress on the `run_inference` function that is used during
    dataset generation.

--- src/harm_refuse/utils/inference.py
+++ src/harm_refuse/utils/inference.py
@@ -1,5 +1,5 @@
 """
-Utilities for loading and using models.
+Utilities for model inference.
 """
 from nnsight import LanguageModel
 from torch import Tensor
@@ -11,18 +11,29 @@ def _tokenize(prompt:str, model: LanguageModel) -> Tensor:
     Applies chat template and then tokenizes.
     """
     return model.tokenizer.apply_chat_template(
-        converstaion = [{
+        conversation = [{
             "role": "user",
             "content": prompt
         }],
         add_generation_prompt=True,
-    )
+        return_tensors="pt",
+    ) # pyright: ignore[reportReturnType]
 
 def run_inference(
     prompt: str, 
     model: LanguageModel
 ) -> Tuple[Tensor, Tensor, bool]:
     tokens = _tokenize(prompt, model)
-    with model.trace(tokens):
+    with model.generate(tokens, max_new_tokens=256):
+        # Save the residual stream after *one* forward pass
+        resid = model.transformer.h.output.save()
+
+        # Save output tokens after *all* forwards passes
+        response_tokens = model.generator.output.save()
+
+    response_str = model.tokenizer.decode(
+        response_tokens.value[0].tolist()[len(tokens):],
+        skip_special_tokens=True,
+    )
 
     pass
--- test.py
+++
@@ -1,6 +0,0 @@
-from src.harm_refuse.utils.load.prompts import load_sorry_bench
-
-sorrybench = load_sorry_bench()
-for i in sorrybench:
-    print(i)
-    break
